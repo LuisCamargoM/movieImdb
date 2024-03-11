26db59f1e9a716e35aa3a3d9b3cc5e8f7278e315
Luis Camargo <luis.e.c.mendoza@gmail.com>
2024-03-11 00:06:45 -0300

    screen: Details screen

--- src/navigation/stacks/MainStack.tsx
+++ src/navigation/stacks/MainStack.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import InitialScreen from '../../screens/Initial';
+import DetailsScreen from '../../screens/Details';
 
 type MainStackList = {
   Home: undefined;
+  Details: undefined;
 };
 
 const Main = createNativeStackNavigator<MainStackList>();
@@ -11,10 +13,15 @@ const Main = createNativeStackNavigator<MainStackList>();
 export const MainStack = (): JSX.Element => {
   return (
     <Main.Navigator>
-      <Main.Group >
+      <Main.Screen
+        name="Home"
+        component={InitialScreen}
+        options={{headerShown: false}}
+      />
+      <Main.Group screenOptions={{presentation: 'modal'}}>
         <Main.Screen
-          name="Home"
-          component={InitialScreen}
+          name="Details"
+          component={DetailsScreen}
           options={{headerShown: false}}
         />
       </Main.Group>
---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {
+  Dimensions,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  View,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {IText, ITitle} from '../../components/Text';
+const DetailsScreen: React.FC = props => {
+  const {container} = styles;
+  const {item} = props.route.params;
+  const {width, height} = Dimensions.get('window');
+  const description =
+    'Nove famílias nobres lutam pelo controle sobre as terras míticas de Westeros, enquanto um antigo inimigo retorna depois de estar adormecido por milhares de anos.';
+  const actors = item['#ACTORS'].split(',');
+  return (
+    <>
+      <View style={container}>
+        <ImageBackground
+          source={{uri: item['#IMG_POSTER']}}
+          resizeMode="cover"
+          style={{
+            width: width,
+            height: height / 2,
+            flex: 1,
+          }}>
+          <LinearGradient
+            colors={['transparent', 'rgba(25,25,25,1)']}
+            style={{
+              height: height / 2,
+            }}
+          />
+        </ImageBackground>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            //
+          }}>
+          {/* <Image
+            source={{uri: item['#IMG_POSTER']}}
+            width={250}
+            height={300}
+            // style={{top: -height / 2.8}}
+          /> */}
+          <ScrollView
+            style={{
+              flex: 1,
+              //   top: -height / 3.8,
+            }}>
+            <View style={{flexDirection: 'row'}}>
+              <ITitle text={`${item['#TITLE']}`} color="white" size={40} />
+            </View>
+            <View style={{marginLeft: 5, marginTop: 10}}>
+              <IText
+                text={item['#YEAR']}
+                color="grey"
+                size={18}
+                adjustsFontSizeToFit
+              />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 20,
+                marginBottom: 20,
+                alignItems: 'flex-end',
+              }}>
+              {actors?.map(actor => {
+                return (
+                  <View
+                    style={{
+                      paddingHorizontal: 5,
+                      marginRight: 10,
+                      paddingVertical: 5,
+                      borderRadius: 5,
+                      borderWidth: 0.5,
+                      borderColor: 'grey',
+                    }}>
+                    <IText text={actor} color="grey" size={14} />
+                  </View>
+                );
+              })}
+            </View>
+            <View style={{flexDirection: 'column', marginVertical: 20}}>
+              <IText text={'Description: '} color="white" />
+
+              <IText
+                text={description}
+                color="grey"
+                size={14}
+                adjustsFontSizeToFit
+              />
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 500,
+    flexDirection: 'column',
+    backgroundColor: '#191919',
+  },
+});
+
+export default DetailsScreen;
