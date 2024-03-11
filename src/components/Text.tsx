f00df3c81d597df19ba5a3d5ab090e39029d21f7
Luis Camargo <luis.e.c.mendoza@gmail.com>
2024-03-11 00:07:26 -0300

    component: added som fixes

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Text} from 'react-native';
+import {Text} from 'react-native';
 
 type CustomTextProps = {
   text: string;
   type?: 'title' | 'label' | 'text';
+  color: string;
 } & React.ComponentProps<any>;
 
-const IText: React.FC<CustomTextProps> = ({text, ...rest}) => {
-  return <Text {...rest}>{text}</Text>;
+const IText: React.FC<CustomTextProps> = ({text, color, size, ...rest}) => {
+  return (
+    <Text style={{color, fontSize: size ?? 16, fontWeight: 'bold'}} {...rest}>
+      {text}
+    </Text>
+  );
 };
 
-const ITitle: React.FC<CustomTextProps> = ({text, ...rest}) => {
+const ITitle: React.FC<CustomTextProps> = ({text, color, size, ...rest}) => {
   return (
-    <Text
-      {...rest}
-      style={{fontSize: 20, fontWeight: 'bold', color: '#292929'}}>
+    <Text {...rest} style={{fontSize: size ?? 24, fontWeight: 'bold', color}}>
       {text}
     </Text>
   );
