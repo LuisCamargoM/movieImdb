855283f974a8a1004e0ba2c286ed6195b848e60a
Luis Camargo <luis.e.c.mendoza@gmail.com>
2024-03-11 03:15:44 -0300

    added last screen info

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Dimensions,
   ImageBackground,
@@ -8,13 +8,23 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {IText, ITitle} from '../../components/Text';
+import {getMovieDetails} from '../../services/api';
+import {Details} from '../../services/types';
 const DetailsScreen: React.FC = props => {
   const {container} = styles;
   const {item} = props.route.params;
   const {width, height} = Dimensions.get('window');
-  const description =
-    'Nove famílias nobres lutam pelo controle sobre as terras míticas de Westeros, enquanto um antigo inimigo retorna depois de estar adormecido por milhares de anos.';
-  const actors = item.actors.split(',');
+  const [details, setDetails] = useState<Details>();
+
+  const actors = item?.actors.split(',');
+
+  const fetchMovieDetails = async () => {
+    const result = await getMovieDetails(item?.imdbId);
+    setDetails(result.details);
+  };
+  useEffect(() => {
+    fetchMovieDetails();
+  }, []);
   return (
     <>
       <View style={container}>
@@ -43,51 +53,103 @@ const DetailsScreen: React.FC = props => {
           <ScrollView
             style={{
               flex: 1,
+              marginHorizontal: 20,
             }}>
-            <View style={{flexDirection: 'row'}}>
-              <ITitle text={`${item.title}`} color="white" size={40} />
+            {details?.name && (
+              <View style={{flexDirection: 'row'}}>
+                <ITitle text={`${details?.name}`} color="white" size={40} />
+              </View>
+            )}
+            <View style={{marginTop: 15, flexDirection: 'row'}}>
+              {item?.year && (
+                <IText
+                  text={item.year}
+                  color="grey"
+                  size={18}
+                  adjustsFontSizeToFit
+                />
+              )}
+              {details?.rating && (
+                <IText
+                  text={` | Rating: ${details.rating}`}
+                  color="grey"
+                  size={18}
+                  adjustsFontSizeToFit
+                />
+              )}
+              {details?.duration && (
+                <IText
+                  text={` | Duration: ${details?.duration
+                    .split('PT')[1]
+                    .toLowerCase()}`}
+                  color="grey"
+                  size={18}
+                  adjustsFontSizeToFit
+                />
+              )}
             </View>
-            <View style={{marginLeft: 5, marginTop: 10}}>
-              <IText
-                text={item.year}
-                color="grey"
-                size={18}
-                adjustsFontSizeToFit
-              />
-            </View>
+            {actors.length > 0 && (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginTop: 10,
+                  alignItems: 'flex-end',
+                }}>
+                {actors.length > 0 &&
+                  actors?.map(actor => {
+                    return (
+                      actor && (
+                        <View
+                          style={{
+                            paddingHorizontal: 5,
+                            marginRight: 10,
+                            paddingVertical: 5,
+                            borderRadius: 5,
+                            borderWidth: 0.5,
+                            borderColor: 'grey',
+                          }}>
+                          <IText text={actor} color="grey" size={14} />
+                        </View>
+                      )
+                    );
+                  })}
+              </View>
+            )}
             <View
               style={{
-                flexDirection: 'row',
-                marginTop: 20,
-                marginBottom: 20,
-                alignItems: 'flex-end',
+                flexDirection: 'column',
+                marginVertical: 30,
               }}>
-              {actors?.map(actor => {
-                return (
-                  <View
-                    style={{
-                      paddingHorizontal: 5,
-                      marginRight: 10,
-                      paddingVertical: 5,
-                      borderRadius: 5,
-                      borderWidth: 0.5,
-                      borderColor: 'grey',
-                    }}>
-                    <IText text={actor} color="grey" size={14} />
-                  </View>
-                );
-              })}
-            </View>
-            <View style={{flexDirection: 'column', marginVertical: 20}}>
               <IText text={'Description: '} color="white" />
-
-              <IText
-                text={description}
-                color="grey"
-                size={14}
-                adjustsFontSizeToFit
-              />
+              {details?.description && (
+                <View style={{marginVertical: 5}}>
+                  <IText
+                    text={details?.description}
+                    color="grey"
+                    size={14}
+                    adjustsFontSizeToFit
+                  />
+                </View>
+              )}
             </View>
+            <IText text={'Keywords '} color="white" />
+            {details?.keywords && (
+              <View
+                style={{
+                  flexDirection: 'row',
+                }}>
+                <IText
+                  text={details?.keywords}
+                  style={{
+                    flex: 1,
+                    flexWrap: 'wrap',
+                    color: 'grey',
+                    fontSize: 15,
+                  }}
+                  adjustsFontSizeToFit
+                />
+              </View>
+            )}
           </ScrollView>
         </View>
       </View>
